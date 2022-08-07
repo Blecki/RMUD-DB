e127d1c0580594289edeba7b2e4ef0b1bd0af067
Blecki <1297068+Blecki@users.noreply.github.com>
2022-08-06 21:02:58 -0400

    Create github_test.cs

---
+++ github_test.cs
@@ -0,0 +1,7 @@
+using RMUD;
+using static RMUD.Core;
+
+public class github_test : RMUD.MudObject
+{
+    public github_test() : base("test", "This object only exists to test the github database.") { }
+}
